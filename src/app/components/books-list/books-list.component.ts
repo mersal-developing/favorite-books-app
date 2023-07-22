4bfe7d9e7d6131dd0258e2be8ab1717b3c4fa859
mersal-developing <mersal.developing@gmail.com>
2023-07-22 11:56:40 +0400

    finish layout of book list page

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { Book } from 'src/app/types/types';
 
 @Component({
   selector: 'app-books-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatTableModule],
   templateUrl: './books-list.component.html',
   styleUrls: ['./books-list.component.scss']
 })
 export class BooksListComponent {
+  title = 'My Favorite Books App';
+  displayedColumns: string[] = ['Book Title', 'Year', 'Author name'];
+  columnsToDisplay: string[] = this.displayedColumns.slice();
+  data: Book[] = [];
+
+
+
 
 }
